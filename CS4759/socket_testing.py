94ce60de866a4dbac817675d516d093cf2466bd1
aizaz-iqbal <aizaziqbal96@gmail.com>
2018-03-31 14:19:40 -0230

    It sends mail now

--- CS4759/socket_testing.py
+++ CS4759/socket_testing.py
@@ -5,16 +5,10 @@ import base64
 import ssl
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s = ssl.wrap_socket(s,ssl_version=ssl.PROTOCOL_TLS)
 mail_server = 'smtp.gmail.com'
-#port = 465
-#mail_server = 'smtp-mail.outlook.com'
 port = 587
-#print(host)
-#request = "GET / HTTP/1.1\nHost: "+host_ip+"\n\n"
 
 s.connect((mail_server,port))
-#s.send(request.encode())
 result = s.recv(2048)
 result = result.decode()
 if result[:3] != '220':
@@ -38,17 +32,14 @@ print(tlsr)
 
 s = ssl.wrap_socket(s,ssl_version=ssl.PROTOCOL_TLS)
 
-
 username = "aizaztesting@gmail.com"
 password = "StrongPassword12"
-base64_str = ("\x00"+username+"\x00"+password).encode()
-base64_str = base64.b64encode(base64_str)
-#base64_str =
-authMsg = "AUTH PLAIN ".encode()+base64_str+"\r\n".encode()
-s.send(authMsg)
+b64_auth = ("\x00"+username+"\x00"+password).encode()
+b64_auth = base64.b64encode(b64_auth)
+auth = "AUTH PLAIN ".encode()+b64_auth+"\r\n".encode()
+s.send(auth)
 recv_auth = s.recv(2048)
-print("auth response: "+recv_auth.decode())
-
+print("AUTH response: "+recv_auth.decode())
 
 From = "MAIL FROM: <testingISFUN@yubelikethis.ca>\r\n"
 s.send(From.encode())
@@ -56,7 +47,7 @@ r3 = s.recv(2048)
 r3 = r3.decode()
 print("from response:"+r3)
 
-to = "RCPT TO: <aizaztesting@gmail.com>\r\n"
+to = "RCPT TO: <aizaz_iqbal@hotmail.com>\r\n"
 s.send(to.encode())
 r4 = s.recv(2048)
 r4 = r4.decode()
@@ -68,19 +59,16 @@ r5 = s.recv(2048)
 r5 = r5.decode()
 print("Data response: "+r5)
 
-data = "Testing DAta"
+subject = "Subject: TESt\r\n"
+s.send(subject.encode())
+
+data = "Testing DAta \r\n"
 s.send(data.encode())
-x = "<CRLF>.<CRLF>"
+x = "\r\n"+'.'+"\r\n"
 s.send(x.encode())
 r5 = s.recv(2048)
 r5 = r5.decode()
 print("Data response: "+r5)
-
-#subject = "SUBJECT: TEST\r\n\r\n"
-#s.send(subject.encode())
-##r6 = s.recv(2048)
-#r6 = r6.decode()
-#print("subject response: "+r6)
 
 quit = "QUIT\r\n"
 s.send(quit.encode())
