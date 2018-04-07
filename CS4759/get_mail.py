44f9554c6c9edcd427bf557045f33fd6f621c877
aizaz-iqbal <aizaziqbal96@gmail.com>
2018-04-07 13:19:32 -0230

    we have mail

--- CS4759/get_mail.py
+++ CS4759/get_mail.py
@@ -6,6 +6,8 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s = ssl.wrap_socket(s,ssl_version=ssl.PROTOCOL_TLS)
 mail_server = 'pop.gmail.com'
 port = 995
+# mail_server = 'imap.gmail.com'
+# port = 993
 
 s.connect((mail_server,port))
 result = s.recv(2048)
@@ -16,6 +18,23 @@ print ("connection result: "+result)
 username = "aizaztesting@gmail.com"
 password = "StrongPassword12"
 
+# login = "a login "+username+' '+password+'\r\n'
+# s.send(login.encode())
+# r = s.recv(4096)
+# r = r.decode()
+# print(r)
+#
+# fetch = "a fetch 1 all\r\n"
+# s.send(fetch.encode())
+# r = s.recv(4096)
+# r = r.decode()
+# print(r)
+#
+# logout = "a logout\r\n"
+# s.send(logout.encode())
+# r = s.recv(4096)
+# r = r.decode()
+# print(r)
 user_msg = "USER "+username+"\r\n"
 s.send(user_msg.encode())
 r = s.recv(4096)
@@ -24,24 +43,29 @@ pass_msg = "PASS "+password+"\r\n"
 s.send(pass_msg.encode())
 r = s.recv()
 print(r.decode())
-
+#
 list = "LIST\r\n"
 s.send(list.encode())
 r = s.recv(4096)
 print(r.decode())
 
-msg_number = input("Input message Number: ")
-msg = "RETR "+msg_number+"\r\n"
+#msg_number = input("Input message Number: ")
+#msg = "RETR "+msg_number+"\r\n"
+msg = "RETR "+'1'+"\r\n"
 s.send(msg.encode())
+
+still_reciving = True
+email = ''
+
 
 while True:
 	r = s.recv(4096)
-	if not r:
+	chunk = r.decode()
+	if r == ' ' or chunk[0] == '.' :
 		break
-	print(r.decode())
-	
+	email = email+chunk
 
-
+print(email)
 
 
 quit = "QUIT\r\n"
